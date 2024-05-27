edbdf2dd0ec597043090b254edaf95d7b3111797
Nirav Gohil <gohilnirav222@gmail.com>
2024-05-28 00:07:42 +0530

    AUTHENTICATION_APIS_CREATED

---
+++ MiddleWares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const { ErrorHandler, TryCatch } = require("../utils/utility.js");
+const {APP_TOKEN} = require('../Config/config.js');
+
+const isAuthenticated = TryCatch((req, res, next) => {
+    const token = req.cookies[APP_TOKEN];
+    console.log(token);
+    if (!token)
+      return next(new ErrorHandler("Please login to access this route", 401));
+  
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  
+    req.user = decodedData._id;
+  
+    next();
+  });
+
+  module.exports = { isAuthenticated };
---
+++ MiddleWares/validator.js
@@ -0,0 +1,2 @@
+import { body, param, validationResult } from "express-validator";
+import { ErrorHandler } from "../utils/utility.js";
